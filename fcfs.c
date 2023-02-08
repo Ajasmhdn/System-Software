a29effa99b62d611f05f75159a4dc74dab761ce4
Ajas Muhammed N <ajasaju404@gmail.com>
2023-02-08 23:40:03 +0530

    without arrival time

--- fcfs.c
+++ fcfs.c
@@ -1,34 +1,37 @@
 #include <stdio.h>
+
 int main()
 {
-    int n;
-    printf("Enter the number of processes");
+    int n, i;
+    printf("Enter the no: of the processes :");
     scanf("%d", &n);
-    int bt[n], wt[n], tat[n], twt, ttat;
-    printf("Enter the burst time of processes");
-    for (int i = 0; i < n; i++)
+    int bt[n], wt[n], tat[n];
+    printf("Enter the waiting time :");
+    for (i = 0; i < n; i++)
     {
+
         scanf("%d", &bt[i]);
     }
     wt[0] = 0;
-    for (int i = 1; i < n; i++)
+
+    for (i = 1; i < n; i++)
     {
+
         wt[i] = wt[i - 1] + bt[i - 1];
     }
-    twt = 0;
-    ttat = 0;
+    int ttat = 0, twt = 0;
     printf("---------------------------------------\n");
     printf("| Process | waiting time |   ta time  |\n");
-    for (int i = 0; i < n; i++)
+    for (i = 0; i < n; i++)
     {
+
         tat[i] = wt[i] + bt[i];
-        twt += wt[i];
-        ttat += tat[i];
+        twt = twt + wt[i];
+        ttat = ttat + tat[i];
         printf("---------------------------------------\n");
-        printf("|    %d    |     %d        |    %d       |\n", i, wt[i], tat[i]);
+        printf("|    P%d    |     %d        |    %d       |\n", i, wt[i], tat[i]);
     }
-    printf("---------------------------------------\n");
-    printf("Average waiting time is %.2f", (float)twt / n);
-    printf("\nAverage turnaround time is %.2f", (float)ttat / n);
-    return 0;
+
+    printf("the average waiting time: %.2f\n", (float)twt / n);
+    printf("the average turnaround time: %.2f", (float)ttat / n);
 }
